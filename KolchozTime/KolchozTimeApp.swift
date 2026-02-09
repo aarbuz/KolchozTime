656cbe1b6f1c7cdd353be71845cd34869810ffa7
aarbuz <norbinniopl@gmail.com>
2026-02-09 01:04:31 +0100

    Initial Commit

---
+++ KolchozTime/KolchozTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KolchozTimeApp.swift
+//  KolchozTime
+//
+//  Created by Norbert Wójcik on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct KolchozTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
